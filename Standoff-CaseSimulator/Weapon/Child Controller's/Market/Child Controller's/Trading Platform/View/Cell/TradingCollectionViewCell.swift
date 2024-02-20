ff142e557b13fb0931ebf122c693b638685f4dcd
EVOLUTION666 <nefedovandrey1997@gmail.com>
2024-02-20 14:15:28 +0300

    Add cells for collection

---
+++ Standoff-CaseSimulator/Weapon/Child Controller's/Market/Child Controller's/Trading Platform/View/Cell/TradingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TradingCollectionViewCell.swift
+//  Standoff-CaseSimulator
+//
+//  Created by Andrey on 20.02.2024.
+//
+
+import UIKit
+
+class TradingCollectionViewCell: UIVCollectionViewCell {
+
+}
